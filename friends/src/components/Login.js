55356995981f7ba24bf5e60ea175e577ac30b592
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-02-25 16:47:07 -0500

    login recieves token

---
+++ friends/src/components/Login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Login = (props) => {
+	const initialState = {
+		username: "",
+		password: ""
+	};
+
+	const [data, setData] = React.useState(initialState);
+
+	const handleInputChange = event => {
+		setData({
+			...data,
+			[event.target.name]: event.target.value
+		});
+	};
+
+	const handleFormSubmit = event => {
+		event.preventDefault();
+		axiosWithAuth()
+			.post("/api/login", {
+				username: data.username,
+				password: data.password
+			})
+            .then(res => {
+                console.log(res);
+				window.localStorage.setItem("token", res.data.payload);
+
+				props.history.push("/protected");
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleFormSubmit}>
+				<input
+					type="text"
+					placeholder="Username"
+					value={data.username}
+					name="username"
+					id="username"
+					onChange={handleInputChange}
+					required
+				/>
+
+				<input
+					type="password"
+					placeholder="Password"
+					value={data.password}
+					name="password"
+					id="password"
+					onChange={handleInputChange}
+					required
+				/>
+
+				<button type="submit">
+					Login
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
