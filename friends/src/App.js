e9a78f54580e526e5625e7bfe79c05168a3f1dc4
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-02-25 19:35:42 -0500

    set up context

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 
+import { FriendsContext } from "./contexts/FriendsContext";
 import Login from "./components/Login";
 import FriendsList from "./components/FriendsList";
 import PrivateRoute from "./components/PrivateRoute";
@@ -8,9 +10,25 @@ import PrivateRoute from "./components/PrivateRoute";
 import './App.css';
 
 function App() {
+
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+		axiosWithAuth()
+			.get("/api/friends")
+			.then(res => {
+				console.log(res.data);
+				setFriends(res.data);
+			})
+			.catch(err => {
+				console.log("error fetching from api", err);
+			});
+	}, []);
+
 	return (
 		<Router>
-			<div className="App">
+      <div className="App">
+        <div><h1>My Friends List</h1></div>
 				<ul>
 					<li>
 						<Link to="/login">Login</Link>
@@ -20,9 +38,11 @@ function App() {
 					</li>
 				</ul>
 				<Switch>
-					<PrivateRoute exact path="/protected" component={FriendsList} />
-					<Route path="/login" component={Login} />
-					<Route component={Login} />
+					<FriendsContext.Provider value={{ friends, setFriends }}>
+						<PrivateRoute exact path="/protected" component={FriendsList} />
+						<Route path="/login" component={Login} />
+						{/* <Route component={Login} /> */}
+					</FriendsContext.Provider>
 				</Switch>
 			</div>
 		</Router>
